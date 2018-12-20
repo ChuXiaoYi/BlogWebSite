32cecaf14595a80e0b86bdf5ba530a33c94a7f6e
chuxiaoyi <chuxiaoyi@mioji.com>
2018-12-20 14:36:33 +0800

    增加mkdown编辑器

--- Post/admin.py
+++ Post/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
 
-admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Category)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'views')
+    list_display_links = ('id', 'title')
+
+
+admin.site.register(Post, PostAdmin)
