32508b5c4c44443c367c6fdfd0b663cf9e78a652
chuxiaoyi <chuxiaoyi@mioji.com>
2018-12-20 14:53:57 +0800

    master

---
+++ Post/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.urls import reverse
+from mdeditor.fields import MDTextField
+
+
+# Create your models here.
+
+class Category(models.Model):
+    """分类"""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """标签"""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    """文章"""
+    title = models.CharField(max_length=100)
+    body = MDTextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+    excerpt = models.CharField(max_length=100, blank=True)  # 文章摘要，可为空
+    category = models.ForeignKey(Category, on_delete=True)  # ForeignKey表示1对多（多个post对应1个category）
+    tags = models.ManyToManyField(Tag, blank=True)
+    views = models.PositiveIntegerField(default=0)  # 阅读量
+
+    class Meta:
+        ordering = ['-created_time']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('Post:detail', kwargs={'pk': self.id})
+
+    def add_views(self):
+        """
+        增加阅读量
+        :return:
+        """
+        self.views += 1
+        self.save(update_fields=['views'])
---
+++ Post/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Post, Category
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from comment.models import Comment
+import markdown
+
+
+def index(request):
+    """
+    主页
+    :param request:
+    :return:
+    """
+    return render(request=request, template_name='Post/index.html')
+
+
+def list(request):
+    post = Post.objects.all()
+    for p in post:
+        p.body = markdown.markdown(
+            p.body,
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                'markdown.extensions.toc',
+                'markdown.extensions.fenced_code',
+            ]
+        )
+
+    limit = 3
+    paginator = Paginator(post, limit)
+    page = request.GET.get('page', 1)
+    result = paginator.page(page)
+    context = {
+        "post_list": result,
+        "page": page,
+    }
+    return render(request=request, template_name='Post/list.html', context=context)
+
+
+def detail(request, pk):
+    """
+
+    :param request:
+    :param pk: 接收到的文章的主键id
+    :return:
+    """
+    post = Post.objects.get(id=pk)
+    post.add_views()
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+
+    comment_list = Comment.objects.filter(post__id=pk)
+
+    context = {
+        'post': post,
+        'toc': md.toc,
+        'comment_list': comment_list
+    }
+    return render(request, template_name='Post/single.html', context=context)
+
+
+def archives(request, year, month):
+    """
+    按归档搜索
+    :param request:
+    :param year:
+    :param month:
+    :return:
+    """
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    context = {
+        'post_list': post_list
+    }
+    return render(request, template_name='Post/index.html', context=context)
+
+
+def category(request, pk):
+    post_list = Post.objects.filter(category_id=pk)
+    context = {
+        'post_list': post_list
+    }
+    return render(request, template_name='Post/list.html', context=context)
